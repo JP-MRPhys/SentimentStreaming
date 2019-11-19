ac2aad5ecf31747b615c0cedff0fc63fda20dc40
jehill <jehillparikh@gmail.com>
2019-11-19 12:44:45 +0000

    adding the gRPC_client

--- models/gRPC_client.py
+++ models/gRPC_client.py
@@ -1,39 +1,77 @@
 import grpc
 import requests
 import tensorflow as tf
+from models.utils import download_and_load_datasets, create_tokenizer_from_hub_module
+import bert.run_classifier
 
-from serving.tensorflow_serving.apis import predict_pb2 #TODO fux this find the right version for tensorflow 13.1.1
-from serving.tensorflow_serving.apis import prediction_service_pb2_grpc
+
+from tensorflow_serving.apis import predict_pb2 #TODO fux this find the right version for tensorflow 13.1.1
+from tensorflow_serving.apis import prediction_service_pb2_grpc
 
 
-tf.app.flags.DEFINE_string('server', 'localhost:8500',
+tf.app.flags.DEFINE_string('server', '35.232.105.219:8500',
                            'PredictionService host:port')
-tf.app.flags.DEFINE_string('image', '', 'path to image in JPEG format')
 FLAGS = tf.app.flags.FLAGS
-
-
-def main(_):
-
-
-  data = tokenized_data # TODO how to get tokenized_data
-
-  channel = grpc.insecure_channel(FLAGS.server)
-  stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
-  # Send request
-  # See prediction_service.proto for gRPC request/response details.
-  request = predict_pb2.PredictRequest()
-  request.model_spec.name = 'bert'
-  request.model_spec.signature_name = 'bert_predictions'
-  request.inputs['image_ids'].CopyFrom(
-      tf.contrib.util.make_tensor_proto(data.inputs_ids, shape=[1]))
-  request.inputs['image_mask'].CopyFrom(
-      tf.contrib.util.make_tensor_proto(data.input_mask, shape=[1]))
-  request.inputs['segment_ids'].CopyFrom(
-      tf.contrib.util.make_tensor_proto(data.segment_ids, shape=[1]))
-  result = stub.Predict(request, 10.0)  # 10 secs timeout
-
-  print(result)
+tokenizer = create_tokenizer_from_hub_module()
+print("Tokenizer created")
 
 
 if __name__ == '__main__':
-  tf.app.run()
+
+    channel = grpc.insecure_channel(FLAGS.server)
+
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
+
+
+    train_df, test_df = download_and_load_datasets()
+
+    print("Download complete")
+    train = train_df.sample(len(train_df) - 24000)
+
+    print(train.head(5))
+
+    DATA_COLUMN = 'sentence'
+    LABEL_COLUMN = 'polarity'
+      # label_list is the list of labels, i.e. True, False or 0, 1 or 'dog', 'cat'
+    label_list = [0, 1]
+
+      # Use the InputExample class from BERT's run_classifier code to create examples from the data
+    train_InputExamples = train.apply(lambda x: bert.run_classifier.InputExample(guid=None,
+                                                                                   # Globally unique ID for bookkeeping, unused in this example
+                                                                                   text_a=x[DATA_COLUMN],
+                                                                                   text_b=None,
+                                                                                   label=x[LABEL_COLUMN]), axis=1)
+
+    print(train_InputExamples)
+
+      # We'll set sequences to be at most 128 tokens long.
+    MAX_SEQ_LENGTH = 128
+
+      # Convert our train and test features to InputFeatures that BERT understands.
+    train_features = bert.run_classifier.convert_examples_to_features(train_InputExamples, label_list, MAX_SEQ_LENGTH,
+                                                                        tokenizer)
+
+
+    data_features=train_features[0:10]
+
+    input_ids = [x.input_ids for x in data_features]
+    input_mask = [x.input_mask for x in data_features]
+    segment_ids = [x.segment_ids for x in data_features]
+
+    # Send request
+    # See prediction_service.proto for gRPC request/response details.
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'bert'
+    request.model_spec.signature_name = 'bert_predictions'
+    request.inputs['input_ids'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(input_ids, shape=[10, MAX_SEQ_LENGTH]))
+    request.inputs['input_mask'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(input_mask, shape=[10, MAX_SEQ_LENGTH]))
+    request.inputs['segment_ids'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(segment_ids, shape=[10, MAX_SEQ_LENGTH]))
+    result = stub.Predict(request, 100.0)  # 10 secs timeout
+
+    print(result)
+
+
